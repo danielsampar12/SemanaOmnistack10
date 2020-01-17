bf924e5eb7e8b4ad6fb1cfa60843c774935f628e
danielsampar12 <54335160+danielsampar12@users.noreply.github.com>
2020-01-16 21:10:01 -0300

    cadastro de dev

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 //MongoDB (Não relacional)
+const app = express();
+
 mongoose.connect('mongodb+srv://dgsampar:@Root@clustersemanaomnistack-pvlxj.mongodb.net/SemanaOmnistack10?retryWrites=true&w=majority',{
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
